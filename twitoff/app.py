2f7a0f2cf8a94d73941a39d57016a6b81c4463d9
Isaac Grove <isaacgrove333@gmail.com>
2020-03-26 15:58:33 -0400

    works at 1:01:00 follow-along Wednesday

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,13 +1,13 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .twitter import add_or_update_user
 
 def create_app():
     ''' create and configure an instance of the Flask app'''
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['ENV'] = config('ENV')
     DB.init_app(app)
 
     @app.route("/")
@@ -20,5 +20,22 @@ def create_app():
         DB.drop_all()
         DB.create_all()
         return render_template('base.html', title='DB Reset!', users=[])
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None):
+        message=''
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = 'User {} successfully added:'.format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = 'Error adding {}: {}'.format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets, message=message)
+
+
 
     return app
